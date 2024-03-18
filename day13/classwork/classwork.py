ef2c41d5edb38be63d29e1610926e477ad605be7
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-03-18 17:17:55 +0400

    added codes

---
+++ day13/classwork/classwork.py
@@ -0,0 +1,22 @@
+# age = int(input("Please enter your age:"))
+
+# if age >= 0 and age < 18 :
+#     print("you are kid")
+# elif age >= 18 and age < 50 :
+#     print("you are adult")
+# else :
+#     print("you are too old")
+
+
+
+
+
+num = int(input("Enter the number:"))
+
+if num > 0:
+    print("the number is positive")
+elif num < 0 :
+    print("the number is negative")
+else :
+    print("the number is 0")
+    
---
+++ day13/homework/homework.py
@@ -0,0 +1,151 @@
+
+#მომხმარებელს ვთხოვთ შეიყვანოს მისი ასაკი
+
+# age  = int(input("Please enter your age: "))
+
+
+#თუ შეყვანილი ასაკი ნაკლებია 18ზე მომხმარებელს ვეუბნებით რომ ის მცირეწლოვანია
+
+# if age < 18:
+#     print("You are minor") 
+
+
+    #ხოლო თუ მომხმარებლის მიერ შეყვანილი ასაკი მეტია 18 ზე და ნაკლებია 65ზე ვეუბნებით რომ ის ზრდასრული ადამიანია
+
+# elif age > 18 and age < 65:
+#     print("You are an adult")
+
+
+    #თუ ასაკი აღემატება 65ს მაშინ ვეუბნებით რომ ის მოხუცია
+
+# else:
+#     print("You are a senior citizen")
+
+
+
+#მომხმარებელს ვეკითხებით 5 ჯერ რიცხვს და ვამოწმებთ არის თუ არა თითოეული შეყვანილი რიცხვი ან ლუწი ან კენტი 
+
+
+# for i in range(5):
+#     num = int(input("Please enter your number:"))
+#     if num % 2 == 0:
+#         print("Even number")
+#     elif num % 2 != 0:
+#         print("odd number")
+#     else:
+#         print("The number is zero")
+
+
+
+
+
+
+
+
+#მომხმარებელს ვეკითხებით ქულას ტესტის შემდეგ და თუ სიაში მყოფ ქულას შეიყვანს მომხმარებელი მაშინ დაიპრინტება შესაბამისი კომენტარი
+
+# points = ["A", "B", "C", "D","F"]
+# point = input("Enter your point after test: ")
+
+
+#თუ შეყვანილი ქულა უდრის Aს მაშინ იპრინტება Exellent!
+
+# if point == points[0]:
+#     print("Exellent!")
+
+
+#თუ შეყვანილი ქულა უდრის Bს მაშინ იპრინტება Good job!
+
+# elif point == points[1]:
+#     print("Good job!")
+
+
+# თუ შეყვანილი ქულა იდრის Cს მაშინ იპრინტება You passed    
+
+# elif point == points[2]:
+#     print("You passed")
+
+
+#თუ შეყვანილი ქულა უდრის Dს მაშინ იპრინტება You can do better
+
+# elif point == points[3]:
+#     print("You can do better")
+
+
+    #ხოლო თუ უდრის  Fს მაშინ იპრინტება You failed
+
+# else:
+#     print("You failed")
+
+
+
+
+
+
+
+
+#while loopის გამოყენებით დავბეჭდეთ რიცხვები 1 დან 10მდე
+
+
+# num = 0
+# while num <= 10:
+#     print(num)
+#     num +=1
+
+
+
+
+
+
+#მომხმარებელს ვთხოვთ რომ შემოიტანოს რიცხვი
+
+# num = int(input("Enter the number:"))
+
+
+
+
+#თუ შეყვანილი რიცხვი იქნება 1დან 10ის ჩათვლით მაშინ ვუთხრათ რომ გამოვიცანით
+
+# while True:
+#     if num >= 1 and num <= 10:
+#         print("You guessed it")
+#         break
+   
+
+#ხოლო თუ არ არის შეყვანილი რიცხვი 1 დან 10 ის ჩათვლით დიაპაზონში მომხმარებელს ვკითხოთ იქამდე სანამ არ შეიყვანს სწორ რიცხვს 
+    
+    # else:
+    #     num = int(input("Enter the number:"))
+
+
+
+
+#მომხმარებელს ვთხოვთ შეიყვანოს რიცხვი
+
+# num = int(input("Enter a number: "))
+
+
+
+#for ციკლის გამოყენებით ვაჩვენებთ შეყვანილი რიცხვის პირველ 10 ჯერადს
+
+# for i in range(1, 11):
+#     print(num * i)
+
+
+
+
+#for  loopის გამოყენებით გავაკეთეთ უკუთვლა 10 დან 1მდე
+
+# for i in range(10, 0, -1):
+#     print(i)
+    
+        
+
+
+
+
+ 
+    
+    
+
+    
