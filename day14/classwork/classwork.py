8a8c0a91b74e602fbdc0017ae1a2bcef3682bb57
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-03-22 17:24:17 +0400

    added some codes

---
+++ day14/classwork/classwork.py
@@ -0,0 +1,50 @@
+
+
+# age  =  int(input("Please enter your age:"))
+
+# if age < 18:
+#     print("You are minor")
+# elif age >= 18 and age < 65:
+#     print("You are adult")
+# else:
+#     print("You are old")
+
+
+
+
+
+
+# for i in range(5):
+#     number = int(input("Please enter the number:"))
+#     if number % 2 == 0:
+#         print(number,"is even")
+#     else:
+#         print(number, "is odd")
+
+
+
+
+
+
+# grade = int(input("Enter your grade (A,B,C,D or F):"))
+
+
+# if grade == "A":
+#     print("Excellent!")
+# elif grade == "B":
+#     print("Good job")
+# elif grade == "C":
+#     print("You passed")
+# elif grade == "D":
+#     print("You can do better")
+# else:
+#     print("You failed")
+
+
+
+
+# num = 1 
+
+# while num < 10:
+#     print(num)
+#     num = num + 1
