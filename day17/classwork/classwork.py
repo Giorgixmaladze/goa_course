ee9f1113b7976a67bc298eb1545182e0d731761f
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-03-30 20:16:14 +0400

    added some codes

---
+++ day17/classwork/classwork.py
@@ -0,0 +1,30 @@
+# fav_movies = ["the pianist", "titanic", "superman"]
+
+# fav_movie = fav_movies[2]
+
+# print(fav_movie)
+
+
+# name = "dachi"
+
+# first = name[0]
+
+# last = name[4]
+
+# print("name's first letter is",first,"name's last letter is",last)
+
+
+
+
+# lst = ["d","a","c"]
+
+# print(len(lst))
+
+
+
+
+# games = ["minecraft","csgo","roblox"]++
+
+# games[2] = "sololearn"
+
+# print(games)
---
+++ day17/homework/homework.py
@@ -0,0 +1,14 @@
+
+#შევქმენით ცვლადი სადაც შეტანილია სია რომელშიც წერია სტრინგის სახით თამაშების სახელები
+
+games = ["Grand theft auto","counter strike","minecraft","fortnite"]
+
+
+#ჩავანაცვლეთ თამაშის სახელები სხვა თამაშის სახელებით
+games[0] ="codewars"
+games[1] = "kahoot"
+games[2] = "sololearn"
+games[3] = "codemonkey"
+
+#გამოვიტანეთ ეს ყველაფერი print ფუნქციის საშუალებით
+print(games)
