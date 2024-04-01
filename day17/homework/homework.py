a9cf4e9a5b382bdba273d3625669b4e026e51077
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-04-01 18:05:31 +0400

    Update homework.py

--- day17/homework/homework.py
+++ day17/homework/homework.py
@@ -20,49 +20,61 @@
 
 
 #მომხმარებელს შემოვატანინეთ 5 მნიშვნელობა
-number1 = int(input("Enter the first number:"))
-number2 = int(input("Enter the second number:"))
-number3 = int(input("Enter the third number:"))
-number4 = int(input("Enter the fourth number:"))
-number5 = int(input("Enter the fifth number:"))
+# number1 = int(input("Enter the first number:"))
+# number2 = int(input("Enter the second number:"))
+# number3 = int(input("Enter the third number:"))
+# number4 = int(input("Enter the fourth number:"))
+# number5 = int(input("Enter the fifth number:"))
 
 
 #შევქმენით სია სადაც შენახულია მომხმარებლის მიერ შემოტანილი მონაცემები
-numbers = [number1,number2,number3,number4,number5]
+# numbers = [number1,number2,number3,number4,number5]
 
 
-print(numbers)
+# print(numbers)
 
 
 
 #თუ შემოტანილი რიცხვები არის ლუწი ვეუბნებით მომხმარებელს რომ ეს რიცხვი ლუწია ხოლო თუ კენტია ვამბობთ რომ რიცხვი არის
-if numbers[0] % 2 == 0:
-    print("First number is even")
-else:
-    print("the first number is odd")
+# if numbers[0] % 2 == 0:
+#     print("First number is even")
+# else:
+#     print("the first number is odd")
 
-if numbers[1] % 2 == 0:
-    print("Second number is even")
-else:
-    print("the Second number is odd")
-
-
-if numbers[2] % 2 == 0:
-    print("Third number is even")
-else:
-    print("The third number is odd")
-
-if numbers[3] % 2 == 0:
-    print("Fourth number is even")
-else:
-    print("The Fourth number is odd")
-if numbers[4] % 2 == 0:
-    print("Fifth number is even")
-else:
-    print("The fifth number is odd")
+# if numbers[1] % 2 == 0:
+#     print("Second number is even")
+# else:
+#     print("the Second number is odd")
 
 
+# if numbers[2] % 2 == 0:
+#     print("Third number is even")
+# else:
+#     print("The third number is odd")
 
+# if numbers[3] % 2 == 0:
+#     print("Fourth number is even")
+# else:
+#     print("The Fourth number is odd")
+# if numbers[4] % 2 == 0:
+#     print("Fifth number is even")
+# else:
+#     print("The fifth number is odd")
+
+
+
+
+numbers = []
+for i in range(5):
+    numbers.append(int(input("Enter a number: ")))
+
+
+num_list = numbers
+
+
+even_numbers = [num for num in num_list if num % 2 == 0]
+
+print("Even numbers in the list is:", even_numbers)
 
 
 
