4d1af5119621eb926d4c4ebfcedadc88ddb712cc
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-04-04 15:31:10 +0400

    added day18

---
+++ day18/classwork/classwork.py
@@ -0,0 +1,53 @@
+# list = [0,1,2,3,4,5,6,7,8,9,10]
+
+# print(list[:10+1:2])
+
+
+
+# even_numbers = []
+# odd_numbers = []
+
+
+# for i in range(1,10 + 1,):
+#     if i % 2 == 0:
+#         even_numbers.append(i)
+#     else:
+#         odd_numbers.append(i)
+
+# print(even_numbers)
+# print(odd_numbers)
+
+
+
+# numbers = []
+
+# even_numbers = []
+
+# odd_numbers = []
+
+# for i in range(5):
+#     numbers.append(int(input("Enter the number:")))
+#     if i % 2 == 0:
+#         even_numbers.append(i)
+#     else:
+#         odd_numbers.append(i)
+        
+
+# print("even numbers is",even_numbers)
+# print("odd numbers is",odd_numbers)
+
+
+
+# odd_list = []
+# even_list = []
+
+# for i in range(5):
+#     user_num = int(input("Enter number: "))
+    
+#     if user_num % 2 == 0:
+#         even_list.append(user_num)
+#     else:
+#         odd_list.append(user_num)
+        
+# print(odd_list)
+# print(even_list)
---
+++ day18/homework/homework.py
@@ -0,0 +1,113 @@
+#შემოვიტანეთ ცარიელი სია სადაც მომხმარებლის მიერ შემოტანილი რიცხვები შევა
+# numbers = []
+
+
+
+# მომხმარებელს ვთხოვთ რომ შემოიტანოს 5 რიცხვი
+# for i in range(5):
+#     num = float(input("Enter a number: "))
+#     numbers.append(num)
+
+
+
+# გავაკეთოთ სიაში მყოფი რიცხვების ჯამი
+# total = 0
+# for num in numbers:
+#     total += num
+
+
+
+# გამოვიტანოთ ჯამი print ფუნქციის საშუალებით
+# print("The sum of the numbers is:", total)
+
+
+
+
+
+#გავაკეთეთ ცარიელი სია სადაც შევა მომხმარებლის მიერ შემოტანილი რიცხვები
+# numbers = []
+
+
+
+# მომხმარებელს ვთხოვთ შემოიტანოს 10 რიცხვი
+# for i in range(10):
+#     num = float(input("Enter number {}: ".format(i + 1)))
+#     numbers.append(num)
+
+
+
+
+# გავაკეთეთ ახალი ცვლადი სადაც შეინახება უდიდესი რიცხვი
+# largest = numbers[0]
+
+# მოვახდინოთ სიაში იტერაცია რათა გავიგოთ უდიდესი რიცხვი
+# for num in numbers:
+#     if num > largest:
+#         largest = num
+
+
+
+# გამოვიტანოთ ეს რიცხვი print ფუნქციის საშუალებით
+# print("The largest number in the list is:", largest)
+
+
+
+
+#გავაკეთეთ სია სადაც შევიტანეთ 30 დან 50ის ჩათვლით რიცხვები
+# numbers  = list(range(30 , 50 + 1))
+
+
+
+#შევქმენით ცვლადი რისი საშუალებითაც დავითვლით კენტ რიცხვებს
+# odd_numbers = 0
+
+
+
+#გამოვთვალეთ თუ რამდენი კენტი რიცხვი იქნება სიაში
+# for num in numbers: 
+#     if num % 2 == 0:
+#         odd_numbers += 1
+
+
+
+#გამოვიტანეთ ეს ყველაფერი print ფუნქციის საშუალებით
+# print("The count of odd numbers in the list is", odd_numbers)
+
+
+
+
+# დავადეკლარირეთ ცვლადი სადაც არის სია
+# numbers = []
+
+
+
+# სიაში  10 დან 50ის ჩათვლით 4ის ჯერად რიცხვებს
+# for i in range(10 , 50 + 1):
+#     if i % 4 == 0:
+#         numbers.append(i)
+
+
+
+# #გამოგვაქვს ეს ყველაფერი print ფუნქციის საშუალებით
+# print(numbers)
+# print("Reversed list:", numbers[::-1])
+
+
+
+
+
+
+# დავადეკლარირეთ ცვლადი სახელად even_numbers_with_indexes
+# even_numbers_with_indexes = []
+
+
+
+# შევიტანეთ სიაში ლუწი რიცხვები და გავუწერეთ თიოთოეულ რიცხვს შესაბამისი ინდექსები
+# for i in range(50, 101):
+#     if i % 2 == 0:
+#         even_numbers_with_indices.append("{} - {}".format(i, i - 50))
+
+
+
+# გამოვიტანეთ ეს ყველაფერი print ფუნქციის საშუალებით
+# print(even_numbers_with_indices)
