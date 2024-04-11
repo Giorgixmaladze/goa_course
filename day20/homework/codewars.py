56160f62b70815d10e9f195c331f5630df986252
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-04-11 19:13:38 +0400

    added day 20

---
+++ day20/homework/codewars.py
@@ -0,0 +1,61 @@
+# def even_or_odd(number):
+#     if number % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+    
+
+
+# def number_to_string(num):
+#     return str(num)
+
+
+
+# def opposite(number):
+#     return -number
+
+
+
+# def make_negative(number):
+#     if number < 0:
+#         return number
+#     else:
+#         return -number
+
+
+
+
+# def bool_to_word(boolean):
+#     if boolean:
+#         return "Yes"
+#     else:
+#         return "No"
+
+
+
+
+
+# def positive_sum(arr):
+#     return sum(num for num in arr if num > 0)
+
+
+
+# def remove_char(s):
+#     return s[1:-1]
+
+
+
+# def square_sum(numbers):
+#     return sum( num ** 2 for  num in numbers)
+
+
+
+
+# def no_space(x):
+#     return x.replace(" ", "")
+
+
+
+
+# def double_integer(i):
+#     return i*2
