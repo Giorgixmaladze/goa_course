513c5fc44001964bd2d0db70d9a94955134025b4
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-05-17 12:33:00 +0400

    added some codes

---
+++ day28/classwork/classwork.py
@@ -0,0 +1,78 @@
+# def smash(words):
+#     return " ".join(words)
+
+
+# def square_sum(numbers):
+#     sum = 0
+#     for num in numbers:
+#         sum += num ** 2
+#     return sum
+
+
+# def sum_str(a, b):
+#     # happy coding!
+#     if a == "" and b == "":
+#         return "0"
+#     elif a != "" and b == "":
+#         return a
+#     elif a == "" and b != "":
+#         return b
+#     return str(int(a)+int(b))
+
+
+
+# def solution(string):
+    
+#     return string[::-1]
+
+
+
+# def sum_array(a):
+    
+    
+#     total = 0
+#     for num in a:
+#         total += num
+#     return total
+
+
+
+# def remove_exclamation_marks(s):
+    
+#     return s.replace("!", "")
+
+
+
+# def monkey_count(n):
+#     new_list = []
+#     for i in range(0,n):
+#         i = i + 1
+#         new_list.append(i)
+        
+#     return new_list
+
+
+
+
+
+# def diff(a, b):
+    # new_list = []
+    # for i in a:
+    #     if i not in b and i not in new_list:
+    #         new_list.append(i)
+    
+    # for i in b:
+    #     if i not in a and i not in new_list:
+    #         new_list.append(i)
+    # new_list.sort()
+    # return new_list
+
+
+
+# def xo(s):
+#     str = s.lower()
+
+#     if str.count("x") == str.count("o"):
+#         return True
+#     else:
+#         return False
---
+++ day28/homework/homework.py
@@ -0,0 +1,14 @@
+# def get_average(marks):
+
+#     for i in marks:
+#         sum_list = sum(marks)
+#         avg = sum_list//len(marks)
+#         return avg
+
+
+
+# def make_negative( number ):
+#     if number < 0:
+#         return number
+#     else:
+#         return -number
