6d55532cb1b0fe7f167426f37d6042d2a913f0f0
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-05-25 22:12:29 +0400

    added day 32

---
+++ day32/homework/homework.py
@@ -0,0 +1,23 @@
+# def no_odds(values):
+    
+#     list = []
+#     for i in values:
+#         if i % 2==0:
+#             list.append(i)
+#     return list
+
+
+
+# def sum_cubes(n):
+#     # your code here
+#     sum = 0
+#     for i in range(n + 1):
+#         sum += i**3 
+        
+#     return sum
+
+
+
+
+# def number_of_occurrences(element, sample):
+#     return sample.count(element)
